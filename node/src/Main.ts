c9dd0f1e690ba93cbd7864c2b1b4d26763ca8596
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2020-05-28 23:39:39 -0400

    Add CodeOwners and update license

--- node/src/Main.ts
+++ node/src/Main.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) 2019 Bentley Systems, Incorporated. All rights reserved.
-* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { ExportGraphicsInfo, IModelHost, IModelDb, ECSqlStatement, Texture, ViewDefinition3d, GeometricElement } from "@bentley/imodeljs-backend";
 import { Id64Array, DbResult, Logger, LogLevel } from "@bentley/bentleyjs-core";
